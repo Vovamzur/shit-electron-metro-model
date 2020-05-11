400734aa560e5b6f1b1cf18544957e8a9162fe0b
Vovamzur <vovamazur3@gmail.com>
2020-05-11 19:18:26 +0300

    finish

--- src/renderer/components/Application.tsx
+++ src/renderer/components/Application.tsx
@@ -1,10 +1,16 @@
 import * as React from 'react';
 import { hot } from 'react-hot-loader/root';
 import * as fs from 'fs';
+import * as path from 'path';
+import { remote } from 'electron';
 
-const { useState, useEffect, createContext, useContext } = React
-const carriageIcon =
-  `data:image/png;base64,${fs.readFileSync('assets/carriage.png').toString('base64')}`
+const { useState, useEffect, useRef } = React
+
+const uploadIcon = (filePath: string): string =>
+  `data:image/png;base64,${fs.readFileSync(filePath).toString('base64')}`
+
+const carriageIcon = uploadIcon('assets/carriage.png')
+const openCarriageIcon = uploadIcon('assets/open-carriage.png')
 
 type Size = { HEIGHT: number, WIDTH: number }
 enum Rails { FIRST = 200, SECOND = 400 }
@@ -23,14 +29,26 @@ const STATION_DURATION = 2000
 const STATION_TICKS = STATION_DURATION / ANIMATION_TIME
 const STATIONS_COUNT = 5
 const ROAD_LENGTH = (Depos.LEFT - Depos.RIGHT) / (STATIONS_COUNT + 1)
-const STATIONS = Array(STATIONS_COUNT)
-  .fill(Depos.RIGHT + ROAD_LENGTH).map((rPos, index) => rPos + index * ROAD_LENGTH);
+const STATIONS = Array(STATIONS_COUNT).fill(Depos.RIGHT + ROAD_LENGTH)
+  .map((rPos, index) => rPos + index * ROAD_LENGTH);
+const NEW_CARRIAGE_INTERVAL = STATIONS[1] - STATIONS[0] + 10;
+
+const RAIL_STYLE: React.CSSProperties = {
+  height: RAIL.HEIGHT,
+  width: RAIL.WIDTH,
+  right: Depos.RIGHT,
+  zIndex: 1,
+  position: 'absolute',
+  marginLeft: `-${RAIL.WIDTH / 2}px`,
+  marginTop: `-${RAIL.HEIGHT / 2}px`,
+  border: '5px solid black'
+}
 
 const STATION_STYLE: React.CSSProperties = {
   top: TOP_STATION,
   height: STATION.HEIGHT,
   width: STATION.WIDTH,
-  zIndex: 1,
+  zIndex: 2,
   backgroundColor: '#a0a1a1',
   position: 'absolute',
   marginLeft: `-${STATION.WIDTH / 2}px`,
@@ -57,148 +75,248 @@ const DEPO_STYLE: React.CSSProperties = {
   backgroundColor: 'red'
 }
 
-const RAIL_STYLE: React.CSSProperties = {
-  height: RAIL.HEIGHT,
-  width: RAIL.WIDTH,
-  right: Depos.RIGHT,
-  zIndex: 2,
+const TOOLBAR_STYLE: React.CSSProperties = {
   position: 'absolute',
-  marginLeft: `-${RAIL.WIDTH / 2}px`,
-  marginTop: `-${RAIL.HEIGHT / 2}px`,
-  border: '5px solid black'
+  bottom: 0,
+  right: 0,
+  width: 300,
+  height: 50,
+  backgroundColor: 'yellow',
+  display: 'flex'
 }
 
-type State = {
-  isMining: boolean;
-  isFellOnTrack: boolean;
-  brokenCarriageNumber: number | undefined;
-  setFirstRailCarriages: Function;
-  setSecondRailCarriages: Function;
-}
-const initiatState: State = {
-  isMining: false,
-  isFellOnTrack: false,
-  brokenCarriageNumber: undefined,
-  setFirstRailCarriages: (arg: any) => arg,
-  setSecondRailCarriages: (arg: any) => arg
-};
-const MetroContext = createContext<State>(initiatState);
-
-type Carriage = {
+type TCarriage = {
+  right: number,
   rail: Rails,
   number: number,
-  right: number,
+  duration: number;
+  isBroken: boolean;
+  isOpenedDoor: boolean;
 }
 
-type CarriageState = {
-  top: Rails;
-  right: number;
-  timer: NodeJS.Timer | null;
-  duration: number;
-};
-type CarriagePisition = { top: number; right: number; };
-type UseCarriage = (number: number, rail: Rails, context: State) => [CarriagePisition, 1 | -1]
-
-const useCarriage: UseCarriage = (number, rail, context) => {
-  const {
-    isMining, isFellOnTrack, brokenCarriageNumber,
-    setFirstRailCarriages, setSecondRailCarriages
-  } = context;
-  const setCarriages = rail === Rails.FIRST ? setFirstRailCarriages : setSecondRailCarriages;
+const Carriage = ({ rail, number, right, isBroken, isOpenedDoor }: TCarriage) => {
   const direction = rail === Rails.FIRST ? 1 : -1;
-  const initRight = rail === Rails.FIRST ? Depos.RIGHT : Depos.LEFT;
-  const intialState: CarriageState = { top: rail, right: initRight, duration: 0, timer: null };
-  const [state, setState] = useState<CarriageState>(intialState);
+  const style = { ...CARRIAGE_STYLE, top: rail, right}
+  return (
+    <>
+      <p
+        style={{
+          ...style,
+          zIndex: 5,
+          right: style.right - (direction === 1 ? 0 : CARRIAGE.WIDTH - 8)
+        }}>
+          {number}
+        </p>
+      <img
+        style={{
+          ...style,
+          transform: `scaleX(${-direction})`,
+          border: isBroken ? '2px solid red' : ''
+        }}
+        src={isOpenedDoor ? openCarriageIcon : carriageIcon}
+        alt="carriage"
+        />
+    </>
+  );
+}
+
+const getCarriageNumber = (carriages: TCarriage[]): number => Math.max(...carriages.map(c => c.number)) + 1
+const checkIfInDepo = (carriage: TCarriage) => (
+  carriage && (
+    (carriage.rail === Rails.FIRST && carriage.right === (Depos.LEFT - CARRIAGE.WIDTH)) ||
+    (carriage.rail === Rails.SECOND && carriage.right === Depos.RIGHT)
+  ))
+
+const initialCarriages = [
+  { rail: Rails.FIRST, number: 1, right: Depos.RIGHT + CARRIAGE.WIDTH / 2 , duration: 0, isBroken: false, isOpenedDoor: false },
+  { rail: Rails.SECOND, number: 2, right: Depos.LEFT - CARRIAGE.WIDTH / 2, duration: 0, isBroken: false, isOpenedDoor: false }
+]
+
+const insertCarriageOnFirstRail = (carriages: TCarriage[]): [TCarriage[], number] => {
+  const carriageNumber = getCarriageNumber(carriages)
+  return [[ ...carriages, {
+    rail: Rails.FIRST,
+    number: carriageNumber,
+    right: Depos.RIGHT + CARRIAGE.WIDTH / 2,
+    duration: 0,
+    isBroken: false,
+    isOpenedDoor: false,
+  }], carriageNumber]
+}
+
+const insertCarriageOnSecondRail = (carriages: TCarriage[]): [TCarriage[], number] => {
+  const carriageNumber = getCarriageNumber(carriages)
+  return [[ ...carriages, {
+    rail: Rails.SECOND,
+    number: carriageNumber,
+    right: Depos.LEFT - CARRIAGE.WIDTH / 2,
+    duration: 0,
+    isBroken: false,
+    isOpenedDoor: false,
+  }], carriageNumber]
+}
+
+
+const useFsSystem = () => {
+  const writeStreamRef = useRef<fs.WriteStream | undefined>();
+
+  const write = (string: string) => {
+    const date = new Date().toISOString();
+    const line = `${date}\t${string}`;
+    const out = `${line.replace(/[\n\r]\s*/g, '; ')}\n`;
+    if (writeStreamRef.current) writeStreamRef.current.write(out);
+  }
+
+  useEffect(() => {
+    const [directoryName] = remote.dialog.showOpenDialog({ properties: ['openDirectory'] });
+    const absoluetPath = path.join(directoryName, 'log.log');
+    if (!fs.existsSync(absoluetPath)) fs.writeFileSync(absoluetPath, '');
+    const writeStream: fs.WriteStream = fs.createWriteStream(absoluetPath, { flags: 'a' });
+    writeStreamRef.current = writeStream
+    return () => writeStreamRef.current && writeStreamRef.current.close();
+  }, []);
+
+  return write
+}
+
+const Application = () => {
+  const write = useFsSystem();
+  const [isMining, setIsMining] = useState<boolean>(false);
+  const [falledManRail, setFalledManRail] = useState<number | undefined>(undefined);
+  const [rails, setRailCarriages] = useState<TCarriage[]>(initialCarriages);
+
   const animationStep = () => {
-    setState((prevState: CarriageState) => {
-      const absoluteRight = prevState.right + CARRIAGE.WIDTH / 2;
-      const isStation = prevState.duration === STATION_TICKS
-        ? false
-        : (prevState.duration > 0 || STATIONS.includes(absoluteRight))
-      const duration = isStation ? prevState.duration + 1 : 0;
-      const right: number =
-        (direction === 1 && absoluteRight === Depos.LEFT && Depos.RIGHT) ||
-        (direction === -1 && absoluteRight === Depos.RIGHT && Depos.LEFT) ||
-        prevState.right + (isStation ? 0 : (direction * STEP));
-      setCarriages((prevCarriages: Array<Carriage>) =>
-        prevCarriages.map(c => c.number === number ? { ...c, right } : c)
-      )
-      return { ...prevState, right, duration }
+    setIsMining(isMine => {
+      setFalledManRail(falledRail => {
+        setRailCarriages(carriages => {
+          const brokenCarriage = carriages.find(c => c.isBroken);
+          if (brokenCarriage && checkIfInDepo(brokenCarriage)) {
+            write(`Broken carriage with ${brokenCarriage.number} number arrived on depo`);
+            return carriages.filter(c => c.number !== brokenCarriage.number);
+          }
+          if (isMine) {
+            const hiddenCarriagesNubers = carriages.filter(checkIfInDepo).map(c => c.number)
+            if (hiddenCarriagesNubers.length > 0) {
+              write(`Carriages with ${hiddenCarriagesNubers.join(', ')} numbers arrived on depo cause mining`);
+              return carriages.filter(c => !hiddenCarriagesNubers.includes(c.number))
+            }
+          }
+          if (!isMine && carriages.length === 0) {
+            write(`Carriages with numbers ${initialCarriages.map(c => c.number).join(', ')} start`)
+            return initialCarriages
+          }
+          if (!isMine) {
+            const firstRailCarriages = carriages.filter(c => c.rail === Rails.FIRST)
+            if (firstRailCarriages.length === 0) {
+              const [newCarriages, carriageNumber] = insertCarriageOnFirstRail(carriages);
+              write(`Carriage with number ${carriageNumber} starts`)
+              return newCarriages;
+            }
+            const lastFirstRailCarriages = firstRailCarriages.reduce(
+              (last, current) => current.number > last.number ? current : last,
+              firstRailCarriages[0]
+            )
+            if (
+              lastFirstRailCarriages.right - Depos.RIGHT > NEW_CARRIAGE_INTERVAL && 
+              firstRailCarriages.length < STATIONS.length
+            ) {
+              const [newCarriages, carriageNumber] = insertCarriageOnFirstRail(carriages);
+              write(`Carriage with number ${carriageNumber} starts`)
+              return newCarriages;
+            }
+            const secondRailCarriages = carriages.filter(c => c.rail === Rails.SECOND)
+            if (secondRailCarriages.length === 0) {
+              const [newCarriages, carriageNumber] = insertCarriageOnSecondRail(carriages);
+              write(`Carriage with number ${carriageNumber} starts`)
+              return newCarriages;
+            }
+            const lastSecondRailCarriages = secondRailCarriages.reduce(
+              (last, current) => current.number > last.number ? current : last,
+              secondRailCarriages[0]
+            )
+            if (
+              Depos.LEFT - lastSecondRailCarriages.right - CARRIAGE.WIDTH / 2 > NEW_CARRIAGE_INTERVAL && 
+              secondRailCarriages.length < STATIONS.length
+            ) {
+              const [newCarriages, carriageNumber] = insertCarriageOnSecondRail(carriages);
+              write(`Carriage with number ${carriageNumber} starts`)
+              return newCarriages;
+            }
+          }
+          return carriages.map(carriage => {
+            if (carriage.rail === falledRail) return carriage
+            const direction = carriage.rail === Rails.FIRST ? 1 : -1;
+            let isStation = !isMine;
+            if (isStation) {
+              isStation = carriage.duration === STATION_TICKS
+                ? false
+                : (carriage.duration > 0 || STATIONS.includes(carriage.right + CARRIAGE.WIDTH / 2));
+            }
+            const duration = isStation ? carriage.duration + 1 : 0;
+            const right: number =
+              (direction === 1 && carriage.right === (Depos.LEFT - CARRIAGE.WIDTH) && Depos.RIGHT + CARRIAGE.WIDTH / 2) ||
+              (direction === -1 && carriage.right === Depos.RIGHT && Depos.LEFT - CARRIAGE.WIDTH / 2) ||
+              carriage.right + (isStation ? 0 : (direction * STEP));
+            const isOpenedDoor = isStation && !carriage.isBroken && !isMine
+            return { ...carriage, right, duration, isOpenedDoor }
+          })
+        })
+        return falledRail
+      })
+      return isMine
+    })
+  }
+  
+  const brokeSomeCarriage = () => {
+    setRailCarriages(carriages => {
+      if (carriages.some(c => c.isBroken)) {
+        write(`All carriages were fixed`)
+        return carriages.map(c => ({ ...c, isBroken: false }))
+      }
+      const brokenNumber = rails[Math.floor(Math.random() * rails.length)].number
+      write(`Carriage with number ${brokenNumber} was broken`)
+      return carriages.map(c => ({ ...c, isBroken: c.number === brokenNumber }))
+    })
+  }
+
+  const fellManOnSomeRail = () => {
+    setFalledManRail(rail => {
+      if (rail === undefined) {
+        const allRails = [200, 400];
+        const randomRail = allRails[Math.floor(Math.random() * allRails.length)];
+        write(`Men falled on ${randomRail === 200 ? 'first' : 'second'} rail`)
+        return randomRail
+      }
+      write(`Men was clened from ${rail === 200 ? 'first' : 'second'} rail`)
+      return undefined
     })
   }
 
   useEffect(() => {
     const intervalTimer = setInterval(animationStep, ANIMATION_TIME);
-    setState(prevState => ({ ...prevState, timer: intervalTimer }));
-    return () => state.timer ? clearInterval(state.timer) : undefined;
+    return () => clearInterval(intervalTimer);
   }, [])
 
-  return [state, direction]
-}
-
-type CarriageProps = { rail: number, number: number }
-const Carriage = ({ rail, number }: CarriageProps) => {
-  const metroContext = useContext(MetroContext)
-  const [position, direction] = useCarriage(number, rail, metroContext);
-  const { brokenCarriageNumber } = metroContext;
-
+  const brokenRail = falledManRail
+    ? (falledManRail === Rails.FIRST && 'first') || (falledManRail === Rails.SECOND && 'second')
+    : '-'
   return (
-    <img
-      style={{
-        ...CARRIAGE_STYLE,
-        ...position,
-        transform: `scaleX(${-direction})`,
-        border: brokenCarriageNumber === number ? '2px solid red' : ''
-      }}
-      src={carriageIcon}
-      alt="carriage"
-      />
-  );
-}
-
-const Application = () => {
-  const [isMining, setIsMining] = useState<boolean>(false);
-  const [isFellOnTrack, setIsFellOnTrach] = useState<boolean>(false);
-  const [brokenCarriageNumber, setBrokernNumber] = useState<number | undefined>(undefined);
-  const [firstRail, setFirstRailCarriages] = useState<Array<Carriage>>([]);
-  const [secondRail, setSecondRailCarriages] = useState<Array<Carriage>>([]);
-  const context = {
-    isMining,
-    isFellOnTrack,
-    brokenCarriageNumber,
-    setFirstRailCarriages,
-    setSecondRailCarriages
-  }
-
-  useEffect(() => {
-    setFirstRailCarriages(carriages => [
-      ...carriages, { rail: Rails.FIRST, number: 1, right: Depos.RIGHT }
-    ])
-    setSecondRailCarriages(carriages => [
-      ...carriages, { rail: Rails.SECOND, number: 2, right: Depos.LEFT }
-    ])
-  }, []);
-
-  useEffect(() => {
-
-  }, [])
-
-  return (
-    <MetroContext.Provider value={context}>
+    <>
+      <pre>Broken number: {rails.find(c => c.isBroken)?.number || '-'}</pre>
+      <pre>Man fell on {brokenRail} rail</pre>
+      <pre>Message: {isMining ? 'MINIG' : '-'}</pre>
       {STATIONS.map(left => <div key={left} style={{ ...STATION_STYLE, left }} />)}
-      {firstRail.concat(secondRail).map(carriage => <Carriage {...carriage} key={carriage.number} />)}
-      <div style={{ ...RAIL_STYLE, top: Rails.FIRST }} />
-      <div style={{ ...RAIL_STYLE, top: Rails.SECOND }} />
+      {rails.map(carriage => <Carriage {...carriage} key={carriage.number} />)}
+      <div style={{ ...RAIL_STYLE, top: Rails.FIRST, backgroundColor: falledManRail === Rails.FIRST ? 'red' : 'white' }} />
+      <div style={{ ...RAIL_STYLE, top: Rails.SECOND, backgroundColor: falledManRail === Rails.SECOND ? 'red' : 'white' }} />
       <div style={{ ...DEPO_STYLE, right: Depos.RIGHT }} />
       <div style={{ ...DEPO_STYLE, right: Depos.LEFT }} />
-      <div style={{ position: 'absolute', bottom: 0, right: 0, width: 300, backgroundColor: 'yellow' }}>
+      <div style={TOOLBAR_STYLE}>
         <button onClick={() => setIsMining(prev => !prev)}>mining</button>
-        <button onClick={() => setIsFellOnTrach(prev => !prev)}>felled man</button>
-        <button onClick={() => setBrokernNumber(prev => prev ? 1 : undefined)}>
-          random brokern carriage
-        </button>
+        <button onClick={fellManOnSomeRail}>felled man</button>
+        <button onClick={brokeSomeCarriage}>random brokern carriage</button>
       </div>
-    </MetroContext.Provider>
+    </>
   );
 }
 
